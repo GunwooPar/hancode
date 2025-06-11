c5a90a266bde8b8e8aa26753d1d340423199a974
DESKTOP-PQ7EEKS\박건우 <paaaaa7895@gmail.com>
2025-06-12 00:05:50 +0900

    file함수 수정(---기준으로 구분)

--- file.py
+++ file.py
@@ -1,20 +1,38 @@
-def get_file():
-    global problems 
-    problems = []
+def parse_problems_from_file():
+    
+    all_problems = []
+    current_problem = {}
     with open('python_easy.txt', 'r', encoding='utf-8') as f:
         for line in f:
-            if '#title' in line:
-                print(line)
-                continue
-            if '#desc' in line:
-                print(line)
-                continue
             if '---' in line:
-                continue    
-            problems.append(line.strip())
-     
-    return problems  
-       
-problems = get_file()
-print(problems)
+                
+                if current_problem:
 
+                    all_problems.append(current_problem)
+                
+                current_problem = {}        # 현재 문제 비우기 
+                continue
+            
+            if '#title' in line:
+                current_problem['title'] = line.replace('#title','').replace(':','').strip()
+                continue
+
+            if '#desc' in line:
+                current_problem['desc'] = line.replace('#desc','').replace(':','').strip()
+                continue
+            
+            if 'content' not in current_problem:
+                current_problem['content'] = []
+            
+            current_problem['content'].append(line.strip())
+
+        if current_problem:
+            all_problems.append(current_problem)
+    
+    return all_problems 
+
+
+
+test= parse_problems_from_file()
+
+print(test)
